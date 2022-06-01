fcd014d88ee081acdf55871c5deaa3c8630cba9b
Orguntay <erolaktepe@gmail.com>
2022-06-01 23:25:22 +0300

    first commit

---
+++ main.rs
@@ -0,0 +1,12 @@
+fn main(){
+    let a = {
+         let b = 5;
+          b+5
+        };
+    println!("5 + 5 toplaminin sonucu: {}", a);
+    let mut b = 0;
+    loop{
+        println!("Hackking: {}",b);
+        b=b+1;
+    }
+}
